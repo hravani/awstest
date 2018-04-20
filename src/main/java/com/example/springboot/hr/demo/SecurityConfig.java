dc08e50aad76f416a1c5afe5fd482568f671f118
hravani <hiten.ravani@gmail.com>
2018-04-20 10:09:10 +1000

    updated

--- src/main/java/com/example/springboot/hr/demo/SecurityConfig.java
+++ src/main/java/com/example/springboot/hr/demo/SecurityConfig.java
@@ -1,24 +1,30 @@
 package com.example.springboot.hr.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
 
 @EnableWebSecurity
+@Configuration
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 	
 	@Autowired
 	private AuthenticationEntryPoint authEntryPoint;
 	
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 		http.cors()
 			.and().csrf()
 			.disable()
+			.requestMatcher(new RequestHeaderRequestMatcher("Authorization"))
 			.authorizeRequests()
 			.antMatchers("/api/version").permitAll()
 			.anyRequest().authenticated()
